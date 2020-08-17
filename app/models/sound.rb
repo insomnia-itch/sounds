8da60f5c0d1c79b769f79ebf3b21ce76ce1315f8
Jelani Woods <jelani@firstdraft.com>
2020-08-16 19:40:33 -0500

    Add annotate

--- app/models/sound.rb
+++ app/models/sound.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: sounds
+#
+#  id         :bigint           not null, primary key
+#  source     :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_sounds_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class Sound < ApplicationRecord
   belongs_to :user
+
+  has_one_attached :audio
 end
